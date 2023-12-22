36066e8f590e97c702fd3daa3a8e96d73ef886d8
mikeacosta <mike@acosta.net>
2023-12-22 11:27:15 -0800

    enrich Serilog

--- PlanIt.API/Program.cs
+++ PlanIt.API/Program.cs
@@ -2,9 +2,13 @@ using PlanIt.API;
 using Serilog;
 using Serilog.Events;
 
+var name = typeof(Program).Assembly.GetName().Name;
+
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
     .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithProperty("Assembly", name)
     .WriteTo.Console()
     .WriteTo.File("logs/planit.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
